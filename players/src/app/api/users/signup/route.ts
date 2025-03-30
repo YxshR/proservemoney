de99db16afce65aeec88f4ed88209a4aac9014a3
Yash <yash123456789.yg@gmail.com>
2025-03-30 05:37:47 +0530

    new folder done changes

---
+++ players/src/app/api/users/signup/route.ts
@@ -0,0 +1,49 @@
+import {connect} from "@/dbConfig/dbconfig"
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+
+
+connect()
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
+
+        console.log(reqBody)
+        const user = User.findOne(email)
+
+        if (user) {
+            return NextResponse.json({error: "User already existed"}, {status:400})
+        }
+
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+
+        new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save()
+        console.log(savedUser)
+
+        return NextResponse.json(
+            {
+                message: "User created Successfully",
+                success: true,
+                savedUser
+            }
+        )
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+            {status:500}
+        )
+    }
+}
---
+++ players/src/models/userModel.js
@@ -0,0 +1,44 @@
+
+import mongoose from "mongoose"
+
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true
+        
+    },
+    email : {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true
+        },
+    password : {
+        type: String,
+        required: [true, "Please provide a password"],
+        select: false
+        
+    },
+    isVerfied:{
+        type:Boolean,
+        default:false
+        
+    },
+
+    isAdmin:{
+        type:Boolean,
+        default:false        
+    },
+
+    forgotPasswordToken: String,
+    forgotPasswordExpires: Date,
+    verifyToken: String,
+    verifyTokenExpires: Date,
+
+})
+
+
+const User =mongoose.model.users || mongoose.model("users", userSchema)
+
+export default User
